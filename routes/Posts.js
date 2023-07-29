dce4a20e7b34ea688af8a3a4270a5ce11f7de8ce
ach02raf <ach02raf@gmail.com>
2023-07-29 14:28:45 +0100

    update post

--- routes/Posts.js
+++ routes/Posts.js
@@ -16,13 +16,13 @@ router.put("/commreaction", PostControllers.commreaction);
 router.put("/addcommentReply", PostControllers.addcommentReply);
 router.post(
   "/PostPublication",
-  upload.fields([{name: "images"}]),
+  // upload.fields([{name: "images"}]),
   PostControllers.PostPublication
 );
 
 router.get("/GetTag", PostControllers.GetTag);
 router.post("/AddTag/:tagn", PostControllers.AddTags);
 router.put("updatePost", PostControllers.upateDatePost);
-router.get("/getPublicationsByID/:id" , PostControllers.getPublicationsByID);
+router.get("/getPublicationsByID/:id", PostControllers.getPublicationsByID);
 
 module.exports = router;
